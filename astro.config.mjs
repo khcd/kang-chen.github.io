246e011beabf07204aab3be2fdbd97b645e61984
Kang <kanghongchen@gmail.com>
2026-02-05 11:23:50 +1100

    change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'kang.fyi',
+  site: 'https://kang.fyi',
   base: '/',
   outDir: './dist',
   publicDir: './public',
